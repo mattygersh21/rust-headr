4c1632bc58a72b1b855f03693fd8a84795caf417
mattygersh21 <dakotaseia@hotmail.com>
2023-01-29 14:54:07 -0600

    * Added lib.rs file. * Added MyResult type. * Added Config struct. * Added get_args()  and run() functions. * Included parse int fn, testing, and validation. * Updated fn main to run get_args() and run().

---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use clap::{App,Arg};
+use std::{error::Error};
+
+type MyResult<T> = Result<T,Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: usize,
+    bytes: Option<usize>
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("rust-headr")
+        .version("0.1.0")
+        .author("Matthew Gerszewski dakotaseia@hotmail.com")
+        .about("Simple clone of head command-line tool.")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input files")
+                .multiple(true)
+                .default_value("-")
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .value_name("LINES")
+                .help("Print specified number of lines of file.")
+                .default_value("10")
+                .conflicts_with("bytes")
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .help("Print specified number of bytes of file.")
+                .takes_value(true)
+        )
+        .get_matches();
+
+    let lines = matches
+        .value_of("lines")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("given line count not allowed -- {}",e))?;
+    
+    let bytes = matches
+        .value_of("btyes")
+        .map(parse_positive_int)
+        .transpose()
+        .map_err(|e| format!("given byte count not allowed -- {}",e))?;
+    
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines: lines.unwrap(),
+        bytes
+    })
+    
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(config);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(n) if n>0 => Ok(n),
+        _ => Err(From::from(val))
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(),3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(),"foo".to_string());
+    
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(),"0".to_string());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = rust_headr::get_args().and_then(rust_headr::run) {
+        eprintln!("{}",e);
+        std::process::exit(1);
+    }
 }
